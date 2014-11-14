3b82119f5f6b5ed1bf53b4dae35d0ca232872509
Amey Parulekar <amey@wirywolf.com>
2014-11-14 17:49:41 +0530

    Added haskell to path

--- zsh-custom/exports.zsh
+++ zsh-custom/exports.zsh
@@ -9,3 +9,9 @@ export GIT_EDITOR='emacsclient -c'
 
 # Preferred editor for local and remote sessions
 export EDITOR='emacsclient -c'
+
+# Add GHC 7.8.3 to the PATH, via http://ghcformacosx.github.io/
+export GHC_DOT_APP="/Applications/GHC.app"
+if [[ ! -z $GHC_DOT_APP ]] then
+    export PATH="${HOME}/.cabal/bin:${GHC_DOT_APP}/Contents/bin:${PATH}"
+fi
