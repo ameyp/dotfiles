79b7aa59a6824342d9c08cd05a7ea9b1cc6c8f8c
Amey Parulekar <amey@wirywolf.com>
2014-12-02 13:40:08 +0530

    Added dates when marking TODO items as done

--- emacs.d/modes/ameyp-org.el
+++ emacs.d/modes/ameyp-org.el
@@ -1,4 +1,9 @@
 ;; ---- org-mode ----------------------------
+
+;; C-c , assigns a priority to a TODO
+;; C-c C-t cycles between states (keywords)
+;; C-c a t shows complete todo list
+
 (require 'org)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 
@@ -10,10 +15,18 @@
 (setq org-default-notes-file "~/.emacs.d/org/journal.org")
 (define-key global-map "\C-cc" 'org-capture)
 
+(setq org-todo-keywords
+      '((sequence "TODO" "REVIEW" "VERIFY" "|" "DONE" "DELEGATED")))
+
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "REVIEW(r)" "VERIFY(v)" "|" "DONE(d)" "DELEGATED(l)")))
+
 (setq org-capture-templates
       '(("t" "Todo" entry (file "~/.emacs.d/org/newgtd.org")
              "* TODO %^{Brief Description} %^g\n%?\nAdded: %U\n %i \n%")
         ("j" "Journal" entry (file+datetree "~/org/journal.org")
-             "* %?\nEntered on %U\n  %i\n  %a")))
+	 "* %?\nEntered on %U\n  %i\n  %a")))
+
+(setq org-agenda-files '("~/Documents/agenda"))
 
 (provide 'ameyp-org)
