e4a25a82bae75d8ea8cfa9d9c4b5cf4ef58ed8ae
Amey Parulekar <amey@wirywolf.com>
2015-01-08 07:57:16 +0530

    Added JSX mode to web mode

--- emacs.d/modes/ameyp-web.el
+++ emacs.d/modes/ameyp-web.el
@@ -9,6 +9,12 @@
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.ftl\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
 
 ;; Indentation settings
 ;; Markup
