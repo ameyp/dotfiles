e7c32792cb3ab96fcc0c1bf902aec0166a626ac5
Amey Parulekar <amey@wirywolf.com>
2015-02-03 15:59:38 +0530

    Added function for encoding html

--- emacs.d/modes/ameyp-web.el
+++ emacs.d/modes/ameyp-web.el
@@ -10,11 +10,15 @@
 (add-to-list 'auto-mode-alist '("\\.ftl\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
-(defadvice web-mode-highlight-part (around tweak-jsx activate)
-  (if (equal web-mode-content-type "jsx")
-      (let ((web-mode-enable-part-face nil))
-        ad-do-it)
-    ad-do-it))
+(add-to-list 'auto-mode-alist '("\\.ejs$" . web-mode))
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+;(defadvice web-mode-highlight-part (around tweak-jsx activate)
+;  (if (equal web-mode-content-type "jsx")
+;      (let ((web-mode-enable-part-face nil))
+;        ad-do-it)
+;    ad-do-it))
 
 ;; Indentation settings
 ;; Markup
@@ -23,5 +27,27 @@
 (setq web-mode-css-indent-offset 2)
 ;; Script
 (setq web-mode-code-indent-offset 2)
+;; JSX
+(setq jsx-indent-level 2)
+
+(defun ameyp-web/html-encode ()
+  (interactive)
+  (let* ((regionp (region-active-p))
+         (beg (and regionp (region-beginning)))
+         (end (and regionp (region-end)))
+         (buf (current-buffer)))
+    (with-temp-buffer
+      (switch-to-buffer (current-buffer) nil t)
+      (rename-buffer " *html encode*" t)
+      (insert-buffer-substring buf beg end)
+      (goto-char (point-min))
+      (replace-string "&" "&amp;")
+      (goto-char (point-min))
+      (replace-string "<" "&lt;")
+      (goto-char (point-min))
+      (replace-string ">" "&gt;")
+      (goto-char (point-min))
+      (replace-string "\"" "&quot;")
+      (clipboard-kill-region (point-min) (point-max)))))
 
 (provide 'ameyp-web)
