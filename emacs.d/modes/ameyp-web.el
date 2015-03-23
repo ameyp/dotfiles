3c0b35130fc4d5304ea9553619e8a239b0870b22
Amey Parulekar <amey@wirywolf.com>
2015-03-23 12:59:06 +0530

    Load freemarker mode for .html files too

--- emacs.d/modes/ameyp-web.el
+++ emacs.d/modes/ameyp-web.el
@@ -14,6 +14,9 @@
 
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
+(setq web-mode-engines-alist
+      '(("freemarker"    . "\\.html\\'")))
+
 ;(defadvice web-mode-highlight-part (around tweak-jsx activate)
 ;  (if (equal web-mode-content-type "jsx")
 ;      (let ((web-mode-enable-part-face nil))
