28161f9a9fd8dad7b0641bb55389da74d8d4ce88
Amey Parulekar <ameyzing@amazon.com>
2017-09-22 13:36:45 -0700

    Attempt to fix company providing downcased candidates

--- dotfiles/emacs/.emacs.d/modes/ameyp-company.el
+++ dotfiles/emacs/.emacs.d/modes/ameyp-company.el
@@ -1,11 +1,12 @@
 ;; Activate company-mode
 (add-hook 'after-init-hook
-	  (lambda ()
-	    (global-company-mode)
-	    (define-key global-map (kbd "C-<tab>") 'company-complete)
-	    (define-key company-active-map (kbd "C-n") 'company-select-next)
-	    (define-key company-active-map (kbd "C-p") 'company-select-previous)
-	    (define-key company-active-map (kbd "C-d") 'company-show-doc-buffer)
-	    (define-key company-active-map (kbd "<tab>") 'company-complete)))
+          (lambda ()
+            (global-company-mode)
+            (setq-local company-dabbrev-downcase nil)
+            (define-key global-map (kbd "C-<tab>") 'company-complete)
+            (define-key company-active-map (kbd "C-n") 'company-select-next)
+            (define-key company-active-map (kbd "C-p") 'company-select-previous)
+            (define-key company-active-map (kbd "C-d") 'company-show-doc-buffer)
+            (define-key company-active-map (kbd "<tab>") 'company-complete)))
 
 (provide 'ameyp-company)
