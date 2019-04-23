74887f5ea32ddd2a549a438fd9e3d184c66dfd4c
Amey Parulekar <ameyzing@amazon.com>
2019-04-23 09:45:20 -0700

    Switch cmake and coffee modes to use-package

--- emacs.d/modes/ameyp-coffee.el
+++ emacs.d/modes/ameyp-coffee.el
@@ -1,13 +1,8 @@
-(autoload 'coffee-mode "coffee-mode" "Major mode for coffeescript files" t)
-(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
-
-(add-hook 'coffee-mode-hook
-          (lambda ()
-	    ;; Activate helm-dash docset
-	    (setq-local helm-dash-docsets '("CoffeeScript" "JavaScript" "NodeJS"))
-
-	    ;; Set tab-width
-            (set (make-local-variable 'tab-width) 4)
-            (set 'coffee-tab-width 4)))
+(use-package coffee-mode
+  :mode "\\.coffee\\'"
+  :config
+  (setq helm-dash-docsets '("CoffeeScript" "JavaScript" "NodeJS")
+	tab-width 4
+	coffee-tab-width 4))
 
 (provide 'ameyp-coffee)
