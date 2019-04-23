8d4ffae7c8f8466058bc806c1763f2916945606f
Amey Parulekar <ameyzing@amazon.com>
2019-04-23 14:58:37 -0700

    Switch haskell-mode to use-package.

--- emacs.d/modules/ameyp-haskell.el
+++ emacs.d/modules/ameyp-haskell.el
@@ -1,38 +1,26 @@
-(autoload 'haskell-mode "haskell-mode" "Major mode for haskell files" t)
-(add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
-
-(add-hook 'haskell-mode-hook
-          (lambda ()
-	    ;; Activate helm-dash docset
-	    (setq-local helm-dash-docsets '("Haskell"))
-
-	    ;; Enable indentation
-					;(turn-on-haskell-simple-indent)
-					;(turn-on-haskell-indent)
-	    (turn-on-haskell-indentation)
-
-	    ;; Set up interactive mode
-	    (custom-set-variables
-	     '(haskell-process-suggest-remove-import-lines t)
-	     '(haskell-process-auto-import-loaded-modules t)
-	     '(haskell-process-log t))
-
-	    ;; Haskell keybindings
-	    (define-key haskell-mode-map (kbd "C-,") 'haskell-move-nested-left)
-	    (define-key haskell-mode-map (kbd "C-.") 'haskell-move-nested-right)
-	    (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
-	    (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
-	    (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
-	    (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
-	    (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-	    (define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-	    (define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
-
-	    ;; Cabal keybindings
-	    ;(define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
-	    ;(define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-	    ;(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-	    ;(define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
-	    ))
+(use-package haskell-mode
+  :mode "\\.hs\\'"
+  :config
+  (setq helm-dash-docsets '("Haskell"))
+  (turn-on-haskell-indentation)
+  :bind (:map haskell-mode-map
+         ("C-," . haskell-move-nested-left)
+         ("C-." . haskell-move-nested-right)
+         ("C-c C-l" . haskell-process-load-or-reload)
+         ("C-`" . haskell-interactive-bring)
+         ("C-c C-t" . haskell-process-do-type)
+         ("C-c C-i" . haskell-process-do-info)
+         ("C-c C-c" . haskell-process-cabal-build)
+         ("C-c C-k" . haskell-interactive-mode-clear)
+         ("C-c c" . haskell-process-cabal)
+         :map haskell-cabal-mode-map
+         ("C-`" . haskell-interactive-bring)
+         ("C-c C-k" . haskell-interactive-mode-clear)
+         ("C-c C-c" . haskell-process-cabal-build)
+         ("C-c c" . haskell-process-cabal))
+  :custom
+  (haskell-process-suggest-remove-import-lines t)
+  (haskell-process-auto-import-loaded-modules t)
+  (haskell-process-log t))
 
 (provide 'ameyp-haskell)
