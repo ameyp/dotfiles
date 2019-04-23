892d1e2d2569c33cf36bf6207b37e2921bd3c470
Amey Parulekar <ameyzing@amazon.com>
2019-04-22 17:46:16 -0700

    Switch clojure-mode to use-package.

--- emacs.d/modes/ameyp-clojure.el
+++ emacs.d/modes/ameyp-clojure.el
@@ -1,26 +1,17 @@
-;; (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
-;; (add-to-list 'auto-mode-alist '("\\.cljs$" . clojure-mode))
-;; (add-to-list 'auto-mode-alist '("\\.clj$" . cider-mode))
-;; (add-to-list 'auto-mode-alist '("\\.cljs$" . cider-mode))
+(use-package clojure-mode
+  :mode "\\.cljs?\\'"
+  :hook (clojure-mode . cider-mode))
 
-;; (autoload 'clojure-mode "clojure-mode" "Major mode for editing clojure files" t)
-;; (autoload 'cider-mode "cider-mode" "Minor mode for clojure repl" t)
+(use-package cider-mode
+  :after (clojure-mode)
+  :config
+  (setq nrepl-hide-special-buffers t)
+  :hook
+  (cider-repl-mode . 'rainbow-delimiters-mode)
+  (cider-repl-mode . 'smartparens-strict-mode))
 
-;(add-hook 'clojure-mode-hook 'cider-mode)
-
-;; Turn on eldoc mode
-(autoload 'cider-eldoc "cider-eldoc" "Eldoc mode for clojure files" t)
-(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-
-;; Enable smartparens
-;; (add-hook 'cider-repl-mode-hook 'smartparens-strict-mode)
-
-;; Enable rainbow delimiters
-(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
-
-(add-hook 'cider-mode-hook
-          (lambda ()
-	    ;; hide nrepl buffers in cider-mode
-            (setq nrepl-hide-special-buffers t)))
+(use-package cider-eldoc
+  :after (cider-mode)
+  :hook (cider-mode . eldoc-mode))
 
 (provide 'ameyp-clojure)
