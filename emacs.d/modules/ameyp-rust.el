27475034864319d88b34bfdeaa67d9af064764c9
Amey Parulekar <ameyzing@amazon.com>
2020-06-20 00:09:37 -0700

    Trying godot-rust because Swift lacks metaprogramming or sane generics.

--- emacs.d/modules/ameyp-rust.el
+++ emacs.d/modules/ameyp-rust.el
@@ -1,20 +1,16 @@
 (use-package lsp-mode
   :commands lsp
   :config
-  (require 'lsp-clients)
-  (setq lsp-enable-snippet nil))
-
-(use-package company-lsp)
+  (require 'lsp-clients))
 
 (use-package rust-mode
-  :ensure
   :mode ("\\.rs\\'" . rust-mode)
   :hook (rust-mode . lsp)
   :config
   (setq helm-dash-docsets '("Rust")))
+  ;(setq indent-tabs-mode nil))
 
-(use-package flycheck-rust
-  :config
-  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+(use-package flycheck
+  :hook (rust-mode . flycheck-mode))
 
 (provide 'ameyp-rust)
