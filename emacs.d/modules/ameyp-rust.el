515782879346b9437a3e0f1b3edf87af1bd71565
Amey Parulekar <ameyzing@amazon.com>
2021-05-28 13:22:26 -0700

    Switch from rust-mode to rustic-mode.

--- emacs.d/modules/ameyp-rust.el
+++ emacs.d/modules/ameyp-rust.el
@@ -1,12 +1,6 @@
-(use-package lsp-mode
-  :ensure t
-  :commands lsp)
-
-(use-package rust-mode
-  :mode ("\\.rs\\'" . rust-mode)
-  :hook (rust-mode . lsp))
-
-(use-package flycheck
-  :hook (rust-mode . flycheck-mode))
+(use-package rustic
+  :config
+  (push 'rustic-clippy flycheck-checkers)
+  )
 
 (provide 'ameyp-rust)
