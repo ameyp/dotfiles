cd6a870d92ed40844953e6690e04e65d64234163
Amey Parulekar <ameyzing@amazon.com>
2021-05-28 13:21:31 -0700

    Add JVM args for LSP to work with Lombok.

--- emacs.d/modules/ameyp-java.el
+++ emacs.d/modules/ameyp-java.el
@@ -1,7 +1,13 @@
 (use-package lsp-java
   :config
   (setq lsp-headerline-breadcrumb-enable nil
-        lsp-java-enable-file-watch nil)
+        lsp-java-enable-file-watch nil
+        lsp-java-vmargs
+        '("-noverify"
+        "-Xmx1G"
+        "-XX:+UseG1GC"
+        "-XX:+UseStringDeduplication"
+        ))
   (add-hook 'java-mode-hook #'lsp))
 
 (provide 'ameyp-java)
