7094847700aa8a657e64fd01729a9486cad92cf6
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:03 -0800

    Clean up text and gui customization.

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -1,3 +1,22 @@
+(put 'set-goal-column 'disabled nil)
+
+;; Disable autosave
+(setq auto-save-default nil)
+
+;; Disable backup
+(setq backup-inhibited t)
+
+;; ---- unzip files in-line from dired ------
+(eval-after-load "dired-aux"
+  '(add-to-list 'dired-compress-file-suffixes
+                '("\\.txt.gz\\'")))
+
+;; Close completions buffer when done with it
+(add-hook 'minibuffer-exit-hook
+          '(lambda ()
+             (let ((buffer "*Completions*"))
+               (and (get-buffer buffer)
+                    (kill-buffer buffer)))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -7,7 +26,7 @@
  '(helm-command-prefix-key "C-c h")
  '(package-selected-packages
    (quote
-    (company-go use-package json-snatcher json-mode go-mode magit yard-mode yaml-mode web-mode smartparens rust-mode robe rainbow-delimiters package-build multiple-cursors markdown-mode lua-mode js2-mode jade-mode inf-ruby helm-projectile helm-dash helm-company helm-cmd-t helm haxe-mode haskell-mode glsl-mode flycheck find-file-in-project expand-region enh-ruby-mode company-tern company coffee-mode cmake-mode cider browse-kill-ring))))
+    (whitespace-toggle-options whitespace-mode company-go use-package json-snatcher json-mode go-mode magit yard-mode yaml-mode web-mode smartparens rust-mode robe rainbow-delimiters package-build multiple-cursors markdown-mode lua-mode js2-mode jade-mode inf-ruby helm-projectile helm-dash helm-company helm-cmd-t helm haxe-mode haskell-mode glsl-mode flycheck find-file-in-project expand-region enh-ruby-mode company-tern company coffee-mode cmake-mode cider browse-kill-ring))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,76 +2,26 @@
 
 (server-start)
 
-;; Remove toolbar, menubar and scrollbar
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-
-;; Inhibit startup screen
-(setq inhibit-startup-screen t)
-
 ;; Add custom paths to 'load-path
 (add-to-list 'load-path "~/.emacs.d/modules")
-
-;; Load package configuration
-(require 'ameyp-packages)
-
-;; Load file that has common functions I use
-(require 'ameyp-common)
-
-;; Set Cofi color theme
-(require 'cofi-color)
-(cofi/colorscheme 'cofi-dark)
-
-;; expand-region
-(require 'expand-region)
-(define-key global-map (kbd "C-=") 'er/expand-region)
-(define-key global-map (kbd "C-+") 'er/contract-region)
-
-;; Set font size
-(if (find-font (font-spec :name "Inconsolata"))
-    (set-face-attribute 'default nil
-                        :font "Inconsolata"))
-
-;; Replace selected text while mark is active
-(delete-selection-mode 1)
-
-(cond ((eq system-type 'darwin)
-       (cond
-        ((string-match "\.amazon\.com" system-name)
-         (set-face-attribute 'default nil :height 160))
-        (t
-         (set-face-attribute 'default nil :height 140))))
-      ((eq system-type 'windows-nt)
-       (set-face-attribute 'default nil :height 130))
-      (t
-       (set-face-attribute 'default nil :height 130)))
-
-;; Keybindings to increase and decrease font size
-(define-key global-map (kbd "M-+")
-  '(lambda () (interactive)
-     (set-face-attribute 'default nil :height (+ (face-attribute 'default :height) 10))))
-(define-key global-map (kbd "M--")
-  '(lambda () (interactive)
-     (set-face-attribute 'default nil :height (- (face-attribute 'default :height) 10))))
-
-;; Indent newline by default
-(define-key global-map (kbd "RET") 'newline-and-indent)
-
-;; Enable flycheck
-(add-hook 'after-init-hook #'global-flycheck-mode)
 
 ;; Disable those god-awful documentation warnings
 ;(eval-after-load 'flycheck (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
 
 ;; Load config files
-(mapcar 'require '(ameyp-clojure
+(mapcar 'require '(;; load the essential packages first
+		   ameyp-packages
+		   ameyp-text
+		   ameyp-gui
+                   ameyp-company
+                   ameyp-helm
+
+		   ;; load language-specific packages next
+		   ameyp-clojure
                    ameyp-cmake
                    ameyp-coffee
-                   ameyp-company
                    ameyp-elisp
                    ameyp-haskell
-                   ameyp-helm
                    ameyp-lua
                    ameyp-markdown
                    ameyp-opengl
@@ -81,52 +31,6 @@
                    ameyp-rust
                    ameyp-web
                    ameyp-yaml))
-
-;; Rainbow Delimiters
-(require 'rainbow-delimiters)
-(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-
-;; Show matching paren
-(show-paren-mode)
-
-;; Disable autosave
-(setq auto-save-default nil)
-
-;; Disable backup
-(setq backup-inhibited t)
-
-;; ---- unzip files in-line from dired ------
-(eval-after-load "dired-aux"
-  '(add-to-list 'dired-compress-file-suffixes
-                '("\\.txt.gz\\'")))
-
-;; Don't highlight current line
-(global-hl-line-mode 0)
-
-;; Tabs are evil
-(setq indent-tabs-mode nil)
-
-;; Before saving:
-;; 1. Untabify if indent-tabs-mode is off
-;; 2. Delete trailing whitespace
-;; 3. Set line-endings to unix
-(add-hook 'before-save-hook
-          (lambda ()
-            (set-buffer-file-coding-system 'utf-8-unix)
-            (delete-trailing-whitespace)
-            (if (not indent-tabs-mode)
-                (untabify (point-min) (point-max)))))
-
-;; Close completions buffer when done with it
-(add-hook 'minibuffer-exit-hook
-          '(lambda ()
-             (let ((buffer "*Completions*"))
-               (and (get-buffer buffer)
-                    (kill-buffer buffer)))))
-
-;; ---- whitespace-mode ---------------------
-(autoload 'whitespace-mode "whitespace" "Toggle whitespace visualization." t)
-(autoload 'whitespace-toggle-options "whitespace" "Toggle local options" t)
 
 ;; Set path
 (if (eq system-type 'windows-nt)
@@ -139,15 +43,6 @@
     (setq exec-path (append exec-path '("~/Apps/bin")))
     (setenv "PATH" (concat (getenv "PATH") ":~/Apps/bin"))))
 
-;; C-x C-m invokes M-x
-(define-key global-map (kbd "C-x C-m") 'execute-extended-command)
-
-(define-key global-map (kbd "C-w") 'backward-kill-word)
-(define-key global-map (kbd "C-x C-k") 'kill-region)
-
-(define-key global-map (kbd "M-s") 'isearch-forward-regexp)
-(define-key global-map (kbd "M-r") 'isearch-backward-regexp)
-(put 'set-goal-column 'disabled nil)
 
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
