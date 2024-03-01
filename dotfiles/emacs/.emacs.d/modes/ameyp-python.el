c97816bb57e8628df8708e951e19a4f8e599294e
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:03 -0800

    Removed external python-mode

--- dotfiles/emacs/.emacs.d/modes/ameyp-python.el
+++ dotfiles/emacs/.emacs.d/modes/ameyp-python.el
@@ -1,4 +1,4 @@
-(autoload 'python-mode "python-mode" "Major mode for editing python files" t)
+(autoload 'python-mode "python" "Major mode for editing python files" t)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 
 (add-hook 'python-mode-hook
