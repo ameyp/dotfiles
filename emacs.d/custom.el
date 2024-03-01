7f524f008611851c69402d0b9d3300a72f60aa27
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:03 -0800

    Use lsp in rust-mode.

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -26,7 +26,7 @@
  '(helm-command-prefix-key "C-c h")
  '(package-selected-packages
    (quote
-    (terraform-mode pry whitespace-toggle-options whitespace-mode company-go use-package json-snatcher json-mode go-mode magit yard-mode yaml-mode web-mode smartparens rust-mode robe rainbow-delimiters package-build multiple-cursors markdown-mode lua-mode js2-mode jade-mode inf-ruby helm-projectile helm-dash helm-company helm-cmd-t helm haxe-mode haskell-mode glsl-mode flycheck find-file-in-project expand-region enh-ruby-mode company-tern company coffee-mode cmake-mode cider browse-kill-ring))))
+    (lsp-ui flycheck-rust company-lsp terraform-mode pry whitespace-toggle-options whitespace-mode company-go use-package json-snatcher json-mode go-mode magit yard-mode yaml-mode web-mode smartparens rust-mode robe rainbow-delimiters package-build multiple-cursors markdown-mode lua-mode js2-mode jade-mode inf-ruby helm-projectile helm-dash helm-company helm-cmd-t helm haxe-mode haskell-mode glsl-mode flycheck find-file-in-project expand-region enh-ruby-mode company-tern company coffee-mode cmake-mode cider browse-kill-ring))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
