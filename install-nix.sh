1848d461f50737aab6bead4e3c601a77acef2aa2
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:04 -0800

    Add fixes for nix bootstrap.

--- install-nix.sh
+++ install-nix.sh
@@ -8,6 +8,8 @@ if ! [[ -x $(command -v nix) ]]; then
     curl -L https://nixos.org/nix/install | sh
 fi
 
+. /home/amey/.nix-profile/etc/profile.d/nix.sh
+
 if ! [[ -x $(command -v home-manager) ]]; then
     ## Old setup
     # Install Home Manager
@@ -21,4 +23,16 @@ if ! [[ -x $(command -v home-manager) ]]; then
     # nix build --no-link .#homeConfigurations.amey.activationPackage
 fi
 
-home-manager switch
+# Fix for https://github.com/nix-community/home-manager/issues/3734
+mkdir -m 0755 -p /nix/var/nix/{profiles,gcroots}/per-user/$USER
+
+# Fix for a conflict for zsh that's been around forever
+sudo rm -rf sudo rm -rf /nix/store/ny9r65799s7xhp605bc2753sjvzkxrrs-nix-2.15.1/share/zsh/site-functions/_nix
+
+rm -f $HOME/.bashrc $HOME/.profile
+
+if [[ $(uname) == "linux" ]]; then
+    home-manager switch --flake "path:$HOME/.dotfiles/nix/nixpkgs#amey@linux" --extra-experimental-features "nix-command flakes"
+else
+    home-manager switch --flake "path:$HOME/.dotfiles/nix/nixpkgs#amey@macos" --extra-experimental-features "nix-command flakes"
+fi
