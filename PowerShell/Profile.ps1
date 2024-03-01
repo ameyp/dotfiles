9b87e1d7d4d331c2baae8ea97161a3cf844745ec
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:03 -0800

    Add installation and configuration for PowerShell core.

---
+++ PowerShell/Profile.ps1
@@ -0,0 +1,29 @@
+# Unbind Alt+num bindings
+Remove-PSReadLineKeyHandler -Chord Alt+0
+Remove-PSReadLineKeyHandler -Chord Alt+1
+Remove-PSReadLineKeyHandler -Chord Alt+2
+Remove-PSReadLineKeyHandler -Chord Alt+3
+Remove-PSReadLineKeyHandler -Chord Alt+4
+Remove-PSReadLineKeyHandler -Chord Alt+5
+Remove-PSReadLineKeyHandler -Chord Alt+6
+Remove-PSReadLineKeyHandler -Chord Alt+7
+Remove-PSReadLineKeyHandler -Chord Alt+8
+Remove-PSReadLineKeyHandler -Chord Alt+9
+
+# Create emacs-like text and history navigation.
+Set-PSReadLineKeyHandler -Chord Ctrl+p -Function PreviousHistory
+Set-PSReadLineKeyHandler -Chord Ctrl+n -Function NextHistory
+Set-PSReadLineKeyHandler -Chord Ctrl+b -Function BackwardChar
+Set-PSReadLineKeyHandler -Chord Alt+b  -Function BackwardWord
+Set-PSReadLineKeyHandler -Chord Ctrl+f -Function ForwardChar
+Set-PSReadLineKeyHandler -Chord Alt+f  -Function ForwardWord
+Set-PSReadLineKeyHandler -Chord Ctrl+a -Function BeginningOfLine
+Set-PSReadLineKeyHandler -Chord Ctrl+e -Function EndOfLine
+
+Set-PSReadLineKeyHandler -Chord Ctrl+u -Function RevertLine
+Set-PSReadLineKeyHandler -Chord Ctrl+k -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::DeleteToEnd()
+    [Microsoft.PowerShell.PSConsoleReadLine]::ForwardChar()
+}
+
+Set-PSReadLineKeyHandler -Chord Ctrl+d -Function DeleteCharOrExit
