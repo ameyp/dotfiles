f5cfeb37000fcb1e487de8e4b3d2f2aca22ef626
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:05 -0800

    Enable betterlockscreen just for linux.

--- nix/nixos/linux.nix
+++ nix/nixos/linux.nix
@@ -2,7 +2,9 @@
 
 {
   home.packages = [
+    pkgs.betterlockscreen
     pkgs.emacs-gtk
+    pkgs.xss-lock
   ];
 
   programs.zsh = {
@@ -14,4 +16,12 @@
   fonts.fontconfig.enable = true;
   targets.genericLinux.enable = true;
   xdg.mime.enable = true;
+
+  # Lockscreen
+  # Initialize it with images by running
+  # betterlockscreen -b /path/to/folder/or/image
+  services.screen-locker = {
+    enable = true;
+    lockCmd = "${pkgs.betterlockscreen}/bin/betterlockscreen -l blur";
+  };
 }
