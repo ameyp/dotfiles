596a329fcf890aca723358a87d2d3d7c51252ef6
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:05 -0800

    Remove org-mode customization from custom.el

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -91,7 +91,7 @@
  '(custom-enabled-themes '(sanityinc-tomorrow-eighties))
  '(custom-safe-themes
    '("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "de1f10725856538a8c373b3a314d41b450b8eba21d653c4a4498d52bb801ecd2" default))
- '(warning-suppress-types '((auto-save) (auto-save) (undo discard-info))))
+ '(warning-suppress-types '((direnv) (auto-save) (auto-save) (undo discard-info))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -99,15 +99,4 @@
  ;; If there is more than one, they won't work right.
  '(bookmark-face ((t (:default))))
  '(ivy-org ((t (:default))))
- ;; '(org-block-begin-line ((t (:height 140 :weight thin :foreground "#888888"))))
- ;; '(org-block-end-line ((t (:height 140 :weight thin :foreground "#888888"))))
- ;; '(org-document-title ((t (:inherit default :weight bold :foreground "#111111" :family "Sans Serif" :height 2.0 :underline nil))))
- ;; '(org-level-1 ((t (:inherit default :weight bold :foreground "#111111" :family "Sans Serif" :height 1.6))))
- ;; '(org-level-2 ((t (:inherit default :weight bold :foreground "#111111" :family "Sans Serif" :height 1.5))))
- ;; '(org-level-3 ((t (:inherit default :weight bold :foreground "#111111" :family "Sans Serif" :height 1.25))))
- ;; '(org-level-4 ((t (:inherit default :weight bold :foreground "#111111" :family "Sans Serif" :height 1.1))))
- ;; '(org-level-5 ((t (:inherit default :weight bold :foreground "#111111" :family "Sans Serif"))))
- ;; '(org-level-6 ((t (:inherit default :weight bold :foreground "#111111" :family "Sans Serif"))))
- ;; '(org-level-7 ((t (:inherit default :weight bold :foreground "#111111" :family "Sans Serif"))))
- ;; '(org-level-8 ((t (:inherit default :weight bold :foreground "#111111" :family "Sans Serif"))))
  '(variable-pitch ((t (:family "Verdana" :height 160 :weight thin)))))
