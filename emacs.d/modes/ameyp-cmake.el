ea6235b084c71c5e7f5a37768cde2da8d0624fed
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:03 -0800

    Switch cmake and coffee modes to use-package

--- emacs.d/modes/ameyp-cmake.el
+++ emacs.d/modes/ameyp-cmake.el
@@ -1,5 +1,4 @@
-;; CMake mode
-(autoload 'cmake-mode "cmake-mode" "Major mode for CMake files" t)
-(add-to-list 'auto-mode-alist '("^CMakeLists\\.txt$" . cmake-mode))
+(use-package cmake-mode
+  :mode "\\^CMakeLists\\.txt$\\'")
 
 (provide 'ameyp-cmake)
