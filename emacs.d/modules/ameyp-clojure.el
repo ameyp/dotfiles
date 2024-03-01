eeade03f3eaee28e2a1cc677f7bfd2f15655a2d3
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:04 -0800

    Clean up cide usage

--- emacs.d/modules/ameyp-clojure.el
+++ emacs.d/modules/ameyp-clojure.el
@@ -2,20 +2,12 @@
   :mode "\\.cljs?\\'"
   :hook (clojure-mode . cider-mode))
 
-(use-package smartparens)
-
-(use-package cider-mode
-  :ensure cider
+(use-package cider
   :after (clojure-mode rainbow-delimiters smartparens)
   :config
   (setq nrepl-hide-special-buffers t)
   :hook
   (cider-repl-mode . 'rainbow-delimiters-mode)
   (cider-repl-mode . 'smartparens-strict-mode))
-
-(use-package cider-eldoc
-  :ensure cider
-  :after (cider-mode)
-  :hook (cider-mode . eldoc-mode))
 
 (provide 'ameyp-clojure)
