642909c177da15c31436bc19e475addb830caa9c
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:04 -0800

    Switch to straight.el

--- emacs.d/modules/ameyp-packages.el
+++ emacs.d/modules/ameyp-packages.el
@@ -1,24 +1,36 @@
-(require 'package)
 (setq package-enable-at-startup nil)
 (setq package-user-dir "~/.emacs.d/.elpa")
 
-(add-to-list 'package-archives
-  '("melpa" . "http://melpa.org/packages/") t)
-(package-initialize)
+;; Bootstrap https://github.com/raxod502/straight.el
+(setq straight-use-package-by-default t)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
+;(add-to-list 'package-archives
+;  '("melpa" . "http://melpa.org/packages/") t)
+;(package-initialize)
+
+(straight-use-package 'use-package)
+
+(straight-use-package 'exec-path-from-shell)
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize))
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(eval-when-compile
-  (require 'use-package))
 
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
 
 (use-package package-build)
+
+(use-package ag)
 
 (provide 'ameyp-packages)
