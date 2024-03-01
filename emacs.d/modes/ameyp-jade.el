807ee5322ab748ccbf0d1e0c69d12338c24c380a
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:03 -0800

    Remove modes I haven't used in a while.

--- emacs.d/modes/ameyp-jade.el
+++
@@ -1,5 +0,0 @@
-(autoload 'sws-mode "sws-mode" "Major mode for significant white-space files" t)
-(autoload 'jade-mode "jade-mode" "Major mode for jade files" t)
-(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
-
-(provide 'ameyp-jade)
