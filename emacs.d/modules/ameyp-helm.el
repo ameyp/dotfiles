11e57ee165fdc5d0cb1fb484d5d9f4f2ea4ad21c
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:03 -0800

    Disable helm-M-x by default because it's slow with lsp turned on.

--- emacs.d/modules/ameyp-helm.el
+++ emacs.d/modules/ameyp-helm.el
@@ -9,38 +9,38 @@
   (custom-set-variables '(helm-command-prefix-key "C-c h"))
   :config
   (setq helm-split-window-in-side-p           nil ; don't open helm buffer inside current window
-	helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
-	helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
-	helm-ff-file-name-history-use-recentf t ; use recent history
-	)
+        helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+        helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+        helm-ff-file-name-history-use-recentf t ; use recent history
+        )
   :bind (("C-c k" . 'helm-show-kill-ring) ;; Helm kill ring
-	 ("C-c i" . 'helm-semantic-or-imenu) ;; Use helm for listing symbols
-	 ("C-x C-f" . 'helm-find-files) ;; Use helm for finding files
-	 ("M-x" . 'helm-M-x) ;; Use helm for M-x
-	 ("C-x C-m" . 'helm-M-x) ;; Use helm for M-x
-	 ("C-x b" . 'helm-mini) ;; Use helm for mini buffer switching
-	 ("C-c h o" . 'helm-occur) ;; Better keybinding for helm occur
-	 ("C-h SPC" . 'helm-all-mark-rings) ;; Global mark rings
-	 :map helm-map
-	 ("C-w" . 'backward-kill-word)
-	 ("C-z" . 'helm-select-action)
-	 ("<tab>" . 'helm-execute-persistent-action)
-	 ("C-i" . 'helm-execute-persistent-action)  ;; make TAB work in terminal
-	 ))
+         ("C-c i" . 'helm-semantic-or-imenu) ;; Use helm for listing symbols
+         ("C-x C-f" . 'helm-find-files) ;; Use helm for finding files
+         ;("M-x" . 'helm-M-x) ;; Use helm for M-x
+         ("C-x C-m" . 'helm-M-x) ;; Use helm for M-x
+         ("C-x b" . 'helm-mini) ;; Use helm for mini buffer switching
+         ("C-c h o" . 'helm-occur) ;; Better keybinding for helm occur
+         ("C-h SPC" . 'helm-all-mark-rings) ;; Global mark rings
+         :map helm-map
+         ("C-w" . 'backward-kill-word)
+         ("C-z" . 'helm-select-action)
+         ("<tab>" . 'helm-execute-persistent-action)
+         ("C-i" . 'helm-execute-persistent-action)  ;; make TAB work in terminal
+         ))
 
 (use-package helm-buffers
   :config
   (setq helm-buffers-fuzzy-matching t) ; fuzzy matching buffer names when non--nil
   :bind (("C-x C-b" . 'helm-buffers-list) ;; Use helm for big buffer switching
-	 :map helm-buffer-map
-	 ("M-d" . 'helm-buffer-run-kill-buffers)))
+         :map helm-buffer-map
+         ("M-d" . 'helm-buffer-run-kill-buffers)))
 
 (use-package helm-grep
   :after (helm)
   :config
   ;; Use ack-grep instead of grep
   (setq helm-grep-default-command "ack -Hn --no-group --no-color %e %p %f"
-	helm-grep-default-recurse-command "ack -H --no-group --no-color %e %p %f"))
+        helm-grep-default-recurse-command "ack -H --no-group --no-color %e %p %f"))
 
 (use-package helm-man
   :after (helm)
@@ -61,8 +61,8 @@
   :config
   (projectile-mode)
   (setq projectile-enable-caching nil
-	projectile-completion-system 'helm
-	projectile-switch-project-action 'helm-projectile))
+        projectile-completion-system 'helm
+        projectile-switch-project-action 'helm-projectile))
 
 (use-package helm-projectile
   :after (projectile)
@@ -71,6 +71,6 @@
   :config
   (helm-projectile-on)
   (setq helm-projectile-sources-list '(helm-source-projectile-projects
-				       helm-source-projectile-files-list)))
+                                       helm-source-projectile-files-list)))
 
 (provide 'ameyp-helm)
