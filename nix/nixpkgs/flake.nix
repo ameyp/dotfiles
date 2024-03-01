c4ebe85c11a8a168ed9d960e23293c02259e0149
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:05 -0800

    Update home-manager to nix-unstable.

--- nix/nixpkgs/flake.nix
+++ nix/nixpkgs/flake.nix
@@ -10,10 +10,9 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
