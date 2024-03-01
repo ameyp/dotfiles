2e9a07bf4b69cab32d24bf963ba212bc9aaf76e5
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:04 -0800

    Stop flycheck from printing errors to messages buffer.

--- emacs.d/modules/ameyp-flycheck.el
+++ emacs.d/modules/ameyp-flycheck.el
@@ -1,5 +1,7 @@
 (use-package flycheck
   :init (global-flycheck-mode)
-  :delight flycheck-mode)
+  :delight flycheck-mode
+  :config
+  (setq flycheck-display-errors-function nil))
 
 (provide 'ameyp-flycheck)
