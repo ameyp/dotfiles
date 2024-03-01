28d5f50a93db7d19ae781fdaf52d03bc440e68c5
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:03 -0800

    Set font size using face attributes instead of setting the whole face.

--- emacs.d/modules/ameyp-gui.el
+++ emacs.d/modules/ameyp-gui.el
@@ -6,22 +6,6 @@
 ;; Inhibit startup screen
 (setq inhibit-startup-screen t)
 
-;; Set font size differently based on OS.
-(cond ((eq system-type 'darwin)
-       (cond
-        ((string-match "\.amazon\.com" system-name)
-         (set-face-font 'default "Hack-14" nil))
-        (t
-         (set-face-font 'default "Hack-13" nil))))
-      (t
-       (set-face-font 'default "Hack-11")))
-
-(setq-default line-spacing 3)
-
-;; Load theme
-(setq custom-theme-directory "~/.emacs.d/themes")
-(load-theme 'brutalist)
-
 ;; Instead of an annoying ding as a bell, flash the modeline instead.
 ;; Disabled because it still causes the rest of the window to flash on windows.
 ;; (defun flash-mode-line ()
@@ -33,5 +17,38 @@
 
 (setq visible-bell nil
       ring-bell-function 'ignore)
+
+;; The order of the next three segments is *extremely* important.
+
+;; 1. Load theme
+(load-theme 'brutalist)
+
+;; 2. Customize theme
+(custom-theme-set-faces
+ 'brutalist
+ `(default ((t (:family "Hack"))))
+ `(fixed-pitch ((t (:family "Hack"))))
+ `(helm-bookmark-file ((t (:inherit default))))
+ `(helm-buffer-file ((t (:inherit default))))
+ `(helm-ff-file ((t (:inherit default))))
+ `(helm-keyword-face ((t (:inherit default))))
+ `(helm-selection ((t (:inherit whitespace-line))))
+ `(lsp-face-highlight-read ((t (:inherit lazy-highlight :underline nil))))
+ `(lsp-face-highlight-write ((t (:inherit lazy-highlight :underline nil :weight bold)))))
+
+;; 3. Set font size and line spacing
+(cond ((eq system-type 'darwin)
+       (cond
+        ((string-match "\.amazon\.com" system-name)
+         (set-face-attribute 'default nil :height 160)
+         (set-face-attribute 'mode-line nil :height 160))
+        (t
+         (set-face-attribute 'default nil :height 130)
+         (set-face-attribute 'mode-line nil :height 130))))
+      (t
+       (set-face-attribute 'default nil :height 110)
+       (set-face-attribute 'mode-line nil :height 110)))
+
+(setq-default line-spacing 3)
 
 (provide 'ameyp-gui)
