5d054968b0cbcf87119b525516371c53027346e0
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:04 -0800

    Disable auto signature display for rust.

--- emacs.d/modules/ameyp-rust.el
+++ emacs.d/modules/ameyp-rust.el
@@ -1,6 +1,13 @@
+;; Install rust-analyzer for this to work.
+;; https://rust-analyzer.github.io/manual.html#rust-analyzer-language-server-binary
+;; Use the cargo method.
+
 (use-package rustic
   :config
   (push 'rustic-clippy flycheck-checkers)
+  ;; Disable function signature documentation windows from popping up automatically.
+  ;; lsp-signature-activate pops it up.
+  (setq lsp-signature-auto-activate nil)
   )
 
 (provide 'ameyp-rust)
