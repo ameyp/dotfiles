809cf474b600758c0f22a5fc662c056b4edd454f
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:03 -0800

    Add PATH customizations for golang.

--- zsh/plugins/go.zsh
+++ zsh/plugins/go.zsh
@@ -1,3 +1,8 @@
 if [[ -d $HOME/Developer && -d $HOME/Developer/go ]]; then
     export GOPATH=$HOME/Developer/go
+    export PATH=$GOPATH/bin:$PATH
+fi
+
+if [[ -d $HOME/Applications && -d $HOME/Applications/go ]]; then
+    export PATH=$HOME/Applications/go/bin:$PATH
 fi
