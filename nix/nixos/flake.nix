94b0956a9946f534541a93bd6d86ba6c2f5b13a8
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:05 -0800

    Start adding nixos configuration.

---
+++ nix/nixos/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "flake for nixOS";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      hyperv = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
