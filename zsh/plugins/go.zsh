bbc73eb90839e933ad29e6df2e9696b7676efdaf
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:04 -0800

    Add more initial config for Go.

--- zsh/plugins/go.zsh
+++ zsh/plugins/go.zsh
@@ -6,3 +6,8 @@ fi
 if [[ -d $HOME/Applications && -d $HOME/Applications/go ]]; then
     export PATH=$HOME/Applications/go/bin:$PATH
 fi
+
+if [[ -d /usr/local/go ]]; then
+    export GOPATH=$HOME/Developer/go
+    export PATH=/usr/local/go/bin:$(/usr/local/go/bin/go env GOPATH)/bin:$PATH
+fi
