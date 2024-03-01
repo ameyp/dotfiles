edf681c97038f9e0ba32d1c46b1984090b16a69d
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:03 -0800

    Commit custom prefix for helm.

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(helm-command-prefix-key "C-c h")
  '(package-selected-packages
    (quote
     (company-go use-package json-snatcher json-mode go-mode magit yard-mode yaml-mode web-mode smartparens rust-mode robe rainbow-delimiters package-build multiple-cursors markdown-mode lua-mode js2-mode jade-mode inf-ruby helm-projectile helm-dash helm-company helm-cmd-t helm haxe-mode haskell-mode glsl-mode flycheck find-file-in-project expand-region enh-ruby-mode company-tern company coffee-mode cmake-mode cider browse-kill-ring))))
