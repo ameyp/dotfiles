1d7bcb98ab22cf6371f371296c45a6f2c26b4324
Amey Parulekar <amey@wirywolf.com>
2024-03-01 13:03:04 -0800

    Add hugo template for org-capture using ox-hugo.

--- emacs.d/modules/ameyp-org.el
+++ emacs.d/modules/ameyp-org.el
@@ -37,12 +37,63 @@
      `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
      `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
      `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.6))))
+     `(ivy-org ((t (:default))))
      `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))
-     `(variable-pitch ((t (:family "Geneva" :height 160 :weight thin))))
+     `(org-block-begin-line ((t (:height 140 :weight thin :foreground "#888888"))))
+     `(org-block-end-line ((t (:height 140 :weight thin :foreground "#888888"))))
+     `(variable-pitch ((t (:family "Verdana" :height 160 :weight thin))))
      ))
 
   (setq org-agenda-files (list "~/Dropbox/org-mode/tasks.org"))
   (setq org-default-notes-file "~/Dropbox/org-mode/tasks.org")
+
+  (defcustom hugo-post-root nil "Path to the folder in which you want Hugo posts to be stored.")
+
+  (defun org-capture-hugo-post ()
+    (if (not hugo-post-root)
+        (customize-set-variable 'hugo-post-root (read-string "Path to Hugo posts root: ")))
+    (let* ((title (read-string "Title: "))
+           (date (format-time-string "%Y-%m-%d"))
+           (slug (downcase
+                  ;; Replace any whitespace in the title with -,
+                  ;; and then remove any non-alphanumeric characters.
+                  (replace-regexp-in-string
+                   "[^a-z0-9\-]" ""
+                   (replace-regexp-in-string "\s" "-" title))))
+
+           ;; Format is YYYY-MM-DD-title-in-lower-case-with-dashes-separating-words.org
+           (file-name (format "%s/%s-%s.org" hugo-post-root date slug))
+           (post-buffer (find-file-noselect file-name)))
+      (with-current-buffer post-buffer
+        (insert "#+draft: true\n")
+        (insert "#+type: post\n")
+        (insert (format "#+title: %s\n" title))
+        (insert (format "#+date: %s\n" date))
+        (insert "#+tags: []\n")
+        )
+      (switch-to-buffer post-buffer)))
+
+  (defun org-hugo-get-all-posts ()
+    (if (not hugo-post-root)
+        (customize-set-variable 'hugo-post-root
+                                (read-string "Path to Hugo posts root: ")))
+    (format "%s/%s" hugo-post-root "all-posts.org"))
+
+  (defun org-hugo-new-subtree-post-capture-template ()
+    "Returns `org-capture' template string for new Hugo post.
+See `org-capture-templates' for more information."
+    (let* ((title (read-from-minibuffer "Post Title: ")) ;Prompt to enter the post title
+           (fname (org-hugo-slug title))
+           (all-posts (format "%s/%s" hugo-post-root "all-posts.org"))
+           )
+      (mapconcat #'identity
+                 `(
+                   ,(concat "* TODO " title)
+                   ":PROPERTIES:"
+                   ,(concat ":EXPORT_FILE_NAME: " fname)
+                   ":END:"
+                   "%?\n")          ;Place the cursor here finally
+                 "\n")))
 
   (setq org-capture-templates
         '(
@@ -53,14 +104,26 @@
            entry (file+datetree "~/Dropbox/org-mode/journal.org")
            "* %(format-time-string \"%H:%M:%S\") %?"
            :empty-lines 1)
+          ("h" "Hugo post"
+           entry
+           (file+olp org-hugo-get-all-posts "Blog Ideas")
+           (function org-hugo-new-subtree-post-capture-template))
 
           ;; ... other templates
           ))
+
+  (setq org-capture-templates (list))
+
+  (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
 
   :bind (("C-c l" . org-store-link)
          ("C-c a" . org-agenda)
          ("C-c c" . org-capture))
   :hook
   (org-mode . visual-line-mode))
+
+(use-package ox-hugo
+  :after ox)
 
 (provide 'ameyp-org)
