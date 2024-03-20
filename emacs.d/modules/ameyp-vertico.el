ba37824f6bb158cb82413e091efdc849053d82b9
Amey Parulekar <amey@wirywolf.com>
2024-03-20 13:13:28 -0700

    Fix ameyp-vertico not having a file ending.

--- emacs.d/modules/ameyp-vertico.el
+++ emacs.d/modules/ameyp-vertico.el
@@ -52,36 +52,36 @@
                 "--line-number "
                 "--search-zip "
                 "--hidden "
-                "--glob=!.git/"))
+                "--glob=!.git/")))
 
-  (use-package marginalia
-    ;; Bind `marginalia-cycle' locally in the minibuffer.
-    :bind
-    (:map minibuffer-local-map
-          ("M-A" . marginalia-cycle))
-    ;; To make the binding available in the *Completions* buffer,
-    ;; add it to the `completion-list-mode-map'.
-    (:map completion-list-mode-map
-          ("M-A" . marginalia-cycle))
+(use-package marginalia
+  ;; Bind `marginalia-cycle' locally in the minibuffer.
+  :bind
+  (:map minibuffer-local-map
+        ("M-A" . marginalia-cycle))
+  ;; To make the binding available in the *Completions* buffer,
+  ;; add it to the `completion-list-mode-map'.
+  (:map completion-list-mode-map
+        ("M-A" . marginalia-cycle))
 
 
-    ;; The :init section is always executed.
-    :init
+  ;; The :init section is always executed.
+  :init
 
-    ;; Marginalia must be activated in the :init section of use-package such that
-    ;; the mode gets enabled right away. Note that this forces loading the
-    ;; package.
-    (marginalia-mode))
+  ;; Marginalia must be activated in the :init section of use-package such that
+  ;; the mode gets enabled right away. Note that this forces loading the
+  ;; package.
+  (marginalia-mode))
 
-  (use-package embark)
+(use-package embark)
 
-  (use-package embark-consult
-    :bind (
-           ("C-c C-o" . embark-export)))
+(use-package embark-consult
+  :bind (
+         ("C-c C-o" . embark-export)))
 
-  (use-package consult-projectile
-    :bind (
-           ("C-," . consult-projectile-find-file)
-           ))
+(use-package consult-projectile
+  :bind (
+         ("C-," . consult-projectile-find-file)
+         ))
 
-  (provide 'ameyp-vertico)
+(provide 'ameyp-vertico)
