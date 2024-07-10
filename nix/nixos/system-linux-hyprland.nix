e4483398f01de3ff1b92dd5d9817599557cfda05
Amey Parulekar <amey@wirywolf.com>
2024-07-10 08:06:04 -0700

    Install android studio.

--- nix/nixos/system-linux-hyprland.nix
+++ nix/nixos/system-linux-hyprland.nix
@@ -11,6 +11,10 @@
   # https://github.com/nix-community/home-manager/issues/3113
   programs.dconf.enable = true;
 
+  environment.systemPackages = with pkgs; [
+    android-studio
+  ];
+
   xdg.portal = {
     enable = true;
     config = {
